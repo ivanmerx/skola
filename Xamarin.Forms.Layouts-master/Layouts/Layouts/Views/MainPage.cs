59875457a2227026a66eaa5f052fc09093ca4531
Ivan Merxbauer <ivan.merxbauer@seznam.cz>
2016-11-28 17:41:36 +0100

    upload

---
+++ Xamarin.Forms.Layouts-master/Layouts/Layouts/Views/MainPage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Layouts.Views
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Choose one of pages"
+                        },
+                        new Button
+                        {
+                            Text = "StackLayout",
+                             Command = showPageCommand(new StackLayoutPage())
+                        },
+                        new Button
+                        {
+                            Text = "AbsoluteLayout",
+                            Command = showPageCommand(new AbsolutePage())
+                        },
+                        new Button
+                        {
+                            Text = "RelativeLayout",
+                             Command = showPageCommand(new RelativeLayoutPage())
+                        },
+                        new Button
+                        {
+                            Text = "Handle user actions",
+                             Command = showPageCommand(new ViewModel())
+                        },
+                        new Button
+                        {
+                            Text = "Absolute + Grid + Styles",
+                            Command = showPageCommand(new CombinatedLayoutPage())
+                        },
+                        new Button
+                        {
+                            Text = "Životopis",
+                            Command = showPageCommand(new Page1())
+                        }
+                    }
+            };
+    }
+
+        Command showPageCommand(Page page)
+        {
+           return new Command((() =>
+           {
+               Navigation.PushAsync(page);
+           }));
+                
+               
+        }
+    }
+}
