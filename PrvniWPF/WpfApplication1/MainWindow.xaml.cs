a144b429cee753d7e53fb9c532a1b16f992a6ae5
Ivan Merxbauer <ivan.merxbauer@seznam.cz>
2016-12-06 17:37:51 +0100

    mozna

--- PrvniWPF/WpfApplication1/MainWindow.xaml.cs
+++ PrvniWPF/WpfApplication1/MainWindow.xaml.cs
@@ -22,64 +22,83 @@ namespace WpfApplication1
     {
         private int _noClicks;
         Otazky otazky = new Otazky();
+        int celkem;
         public MainWindow()
         {
-            Random rnd = new Random();
-            InitializeComponent();
-            var prvnicislo = otazky.prvnicislo();
-            var druhycislo = otazky.druhycislo();
-            var operandy = otazky.operandy();
-            textBlock2.Text = prvnicislo + operandy + druhycislo;
-            int buttony = rnd.Next(1,2);
-    //        textBlock1.Text = buttony.ToString();
-            if (buttony == 2)
-            {
-                button.Content = rnd.Next(1, 200);  
-                switch (operandy)
+            bool won = false;
+       //     while (won == false )
+       //     {
+                Random rnd = new Random();
+                InitializeComponent();
+                var prvnicislo = otazky.prvnicislo();
+                var druhycislo = otazky.druhycislo();
+                var operandy = otazky.operandy();
+                textBlock2.Text = prvnicislo + operandy + druhycislo;
+                int buttony = rnd.Next(1, 2);
+                //        textBlock1.Text = buttony.ToString();
+                if (buttony == 2)
                 {
-                    case "+":
-                        button2.Content = button2.Content = prvnicislo + druhycislo;
-                        break;
-                    case "-":
-                        button2.Content = button2.Content = prvnicislo - druhycislo;
-                        break;
-                    case "*":
-                        button2.Content = button2.Content = prvnicislo * druhycislo;
-                        break;
-                    case "/":
-                        button2.Content = button2.Content = prvnicislo / druhycislo;
-                        break;
-                    default:
-                        button2.Content = "Něco random";
-                        break;
+                    int druhybutton = 2;
+                    button.Content = rnd.Next(1, 200);
+                    switch (operandy)
+                    {
+                        case "+":
+                            button2.Content = button2.Content = prvnicislo + druhycislo;
+                            celkem = prvnicislo + druhycislo;
+                            break;
+                        case "-":
+                            button2.Content = button2.Content = prvnicislo - druhycislo;
+                            celkem = prvnicislo - druhycislo;
+                            break;
+                        case "*":
+                            button2.Content = button2.Content = prvnicislo * druhycislo;
+                            celkem = prvnicislo * druhycislo;
+                            break;
+                        case "/":
+                            button2.Content = button2.Content = prvnicislo / druhycislo;
+                            celkem = prvnicislo / druhycislo;
+                            break;
+                        default:
+                            button2.Content = "Něco random";
+                            break;
 
+                    }
+                    var spravne = button2.Content;
                 }
-            }else if(buttony == 1)
-            {
-                button2.Content = rnd.Next(1, 200);
-                switch (operandy)
+                else if (buttony == 1)
                 {
-                    case "+":
-                        button.Content = button.Content = prvnicislo + druhycislo;
-                        break;
-                    case "-":
-                        button.Content = button.Content = prvnicislo - druhycislo;
-                        break;
-                    case "*":
-                        button.Content = button.Content = prvnicislo * druhycislo;
-                        break;
-                    case "/":
-                        button.Content = button.Content = prvnicislo / druhycislo;
-                        break;
-                    default:
-                        button.Content = "Něco random";
-                        break;
+                    int prvnibutton = 1;
+                    button2.Content = rnd.Next(1, 200);
+                    switch (operandy)
+                    {
+                        case "+":
+                            button.Content = button.Content = prvnicislo + druhycislo;
+                            celkem = prvnicislo + druhycislo;
+                            break;
+                        case "-":
+                            button.Content = button.Content = prvnicislo - druhycislo;
+                            celkem = prvnicislo - druhycislo;
+                            break;
+                        case "*":
+                            button.Content = button.Content = prvnicislo * druhycislo;
+                            celkem = prvnicislo * druhycislo;
+                            break;
+                        case "/":
+                            button.Content = button.Content = prvnicislo / druhycislo;
+                            celkem = prvnicislo / druhycislo;
+                            break;
+                        default:
+                            button.Content = "Něco random";
+                            break;
 
+                    }
+                    var spravne = button.Content;
                 }
             }
-        }
+     //   }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            textBlock1.Text= celkem.ToString();
         }
     }
 }
